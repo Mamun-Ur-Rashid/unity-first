068dc8433b5bb1bd2a2be6408a70830b88621656
Mamun-Ur-Rashid <engmamun656@gmail.com>
2024-03-02 23:20:25 +0600

    rightbar create

--- src/components/layout/RightBar.jsx
+++ src/components/layout/RightBar.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const RightBar = () => {
     return (
-        <div>
-            <h1 className='text-center font-bold'>right bar</h1>
+        <div className='sticky right-0 top-0 z-20 py-8 text-light-1 h-screen flex flex-col gap-12 overflow-auto pl-6 pr-10 max-lg:hidden'>
+            <div className='flex flex-col gap-4'>
+                <h4 className='text-heading4-bold'>Following : </h4>
+                <div className='flex flex-col gap-4'>maping users</div>
+            </div>
+            <div className='flex flex-col gap-4'>
+                <h4 className='text-heading4-bold'>Suggested People :</h4>
+                <div className='flex flex-col gap-4'>maping users</div>
+            </div>
         </div>
     );
 };
